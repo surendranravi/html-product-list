d9c8f69011957f44ab656befe4155141f61b743a
Surendran Ravi <surendranravi1997@gmail.com>
2021-05-19 23:32:47 +0530

    dark mode and search changes

--- html-countries-list/js/main.js
+++ html-countries-list/js/main.js
@@ -1,22 +1,60 @@
 var countryList = [];
 
+var searchCountry = '';
+
+var darkMode = false;
+
+document.getElementById('searchInput').value = searchCountry;
+
+var countryList = [];
+
+
+changeMode();
+
 fetch('https://restcountries.eu/rest/v2/all').then((res)=> {
     res.json().then((res)=>{
-
+      countryList = res;
         showCountryList(res)    
             
     })
 })
 
 
+function changeMode(){
+  
+
+  console.log("darkmode is ", darkMode);
+
+  if(darkMode){
+    document.getElementById('modeBtn').innerHTML = `<i class="fa fa-moon" aria-hidden="true"></i> Dark Mode`;
+    document.body.style.backgroundColor = "#212c36";
+
+  }else{
+    document.getElementById('modeBtn').innerHTML = `<i class="fa fa-moon-o" aria-hidden="true"></i> Dark Mode`
+    document.body.style.backgroundColor = "#fafafa";
+  }
+  
+  
+  darkMode = !darkMode
+
+}
+
+
+function searchCountryList(value){
+  searchCountry = value;
+  showCountryList(countryList);
+}
+
+
 
 function showCountryList(data){
+    var listItem = '';
 
-    var listItem = ''
+    data = data.filter((obj)=> obj.name.toLowerCase().match(searchCountry.toLowerCase()))
 
     data.forEach((obj, index)=> {
 
-        listItem += `  <div class="col-md-3">
+        listItem += `  <div class="col-md-3 d-flex mb-4">
         <div class="box">
         <img src="${obj.flag}">
         <div class="m-3">
