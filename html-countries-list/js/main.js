6c1f55b3a8589e6bb46814411c441deddc21cde5
Surendran Ravi <surendranravi1997@gmail.com>
2021-05-22 00:12:35 +0530

    Dark mode and filter by region

--- html-countries-list/js/main.js
+++ html-countries-list/js/main.js
@@ -1,7 +1,8 @@
 var countryList = [];
 
 var searchCountry = '';
-
+var regionList = [];
+var selectedRegion = '';
 var darkMode = false;
 
 document.getElementById('searchInput').value = searchCountry;
@@ -9,34 +10,45 @@ document.getElementById('searchInput').value = searchCountry;
 var countryList = [];
 
 
+var r = document.querySelector(':root');
+
 changeMode();
+
+
 
 fetch('https://restcountries.eu/rest/v2/all').then((res)=> {
     res.json().then((res)=>{
       countryList = res;
-        showCountryList(res)    
-            
+      var regions = '<option>Filter By Region</option>'
+      res.forEach((obj)=> {
+        if(obj.region && regionList.indexOf(obj.region) <= -1 ){
+          regionList.push(obj.region)
+          regions += `<option value="${obj.region}">${obj.region}</option>`
+        }
+      })
+
+      document.getElementById('region-options').innerHTML = regions;
+      
+      showCountryList(res)    
     })
 })
 
 
+
+
 function changeMode(){
-  
-
-  console.log("darkmode is ", darkMode);
-
   if(darkMode){
-    document.getElementById('modeBtn').innerHTML = `<i class="fa fa-moon" aria-hidden="true"></i> Dark Mode`;
-    document.body.style.backgroundColor = "#212c36";
-
+    document.getElementById('modeBtn').innerHTML = `<span style="color:#f1f7fa;"><i class="fa fa-moon" aria-hidden="true" ></i> Dark Mode</span>`;
+    r.style.setProperty('--backgroud-color', '#212d36');
+    r.style.setProperty('--card-bg-color', '#2b3743');
+    document.body.style.color = "#f1f7fa";
   }else{
-    document.getElementById('modeBtn').innerHTML = `<i class="fa fa-moon-o" aria-hidden="true"></i> Dark Mode`
-    document.body.style.backgroundColor = "#fafafa";
+    document.getElementById('modeBtn').innerHTML = `<span style="color:#16171b;"><i class="fa fa-moon-o" aria-hidden="true"></i> Dark Mode</span>`
+    r.style.setProperty('--backgroud-color', '#fafafa');
+    r.style.setProperty('--card-bg-color', '#fff');
+    document.body.style.color = "#16171b";
   }
-  
-  
   darkMode = !darkMode
-
 }
 
 
@@ -45,12 +57,25 @@ function searchCountryList(value){
   showCountryList(countryList);
 }
 
+function searchByregion(value){
+  selectedRegion = value;
+  if(selectedRegion == 'Filter By Region'){
+    selectedRegion = ''
+  }
+  showCountryList(countryList)
+}
 
 
 function showCountryList(data){
     var listItem = '';
-
-    data = data.filter((obj)=> obj.name.toLowerCase().match(searchCountry.toLowerCase()))
+    data = data.filter((obj)=>  {
+  
+      
+    if((obj.region.toLowerCase().match(selectedRegion.toLowerCase()) && obj.name.toLowerCase().match(searchCountry.toLowerCase()) )  ){
+      return obj         
+    }
+    
+    })
 
     data.forEach((obj, index)=> {
 
@@ -59,14 +84,12 @@ function showCountryList(data){
         <img src="${obj.flag}">
         <div class="m-3">
           <b class="mb-2">${obj.name}</b><br>
-          <span style="font-weight: 500;">Population: <span style="font-weight: 400;color: #7b7b7b;">${obj.population}</span></span><br>
-          <span style="font-weight: 500;">Region: <span style="font-weight: 400;color: #7b7b7b;">${obj.region}</span></span><br>
-          <span style="font-weight: 500;">Capital: <span style="font-weight: 400;color: #7b7b7b;">${obj.capital}</span></span><br><br>
+          <span style="font-weight: 500;">Population: <span style="font-weight: 400;color: #a7b1be;">${obj.population}</span></span><br>
+          <span style="font-weight: 500;">Region: <span style="font-weight: 400;color: #a7b1be;">${obj.region}</span></span><br>
+          <span style="font-weight: 500;">Capital: <span style="font-weight: 400;color: #a7b1be;">${obj.capital}</span></span><br><br>
         </div>
       </div>
       </div>`
-
-        
     })
 
 
